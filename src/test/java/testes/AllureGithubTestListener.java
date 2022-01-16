f05a45741910dea64eb48c1122d7dc060e042732
Nemerteja <aagoncharenko.91@gmail.com>
2022-01-16 14:26:18 +0300

    added Listener

--- src/test/java/testes/AllureGithubTestListener.java
+++ src/test/java/testes/AllureGithubTestListener.java
@@ -2,6 +2,8 @@ package testes;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -17,6 +19,8 @@ public class AllureGithubTestListener {
 
     @Test
     public void TestIssue() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com");
         $(".header-search-input").click();
         $(".header-search-input").sendKeys("allure-starter");
